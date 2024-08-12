22d84d5a47437a5d4d29cedd5871d116469af91d
Juan Gonzalez <j.g.onzalez@accenture.com>
2024-08-11 20:42:56 -0600

    Add Redux Thunk and persisted store to maintain session data, counter, and task list.

--- App.tsx
+++ App.tsx
@@ -1,31 +1,34 @@
 import React from 'react'
-import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
-import {Provider} from 'react-redux'
-import {store} from './src/store/store'
-import {UserProvider} from './src/context/UserContext'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Provider } from 'react-redux'
+import { store, persistor } from './src/store/store'
+import { UserProvider } from './src/context/UserContext'
 import HomeScreen from './src/screens/HomeScreen'
 import UserScreen from './src/screens/UserScreen'
 import LoginScreen from './src/screens/LoginScreen'
 import CounterScreen from './src/screens/CounterScreen'
 import TaskListScreen from './src/screens/TaskListScreen'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const Stack = createNativeStackNavigator()
 
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
-      <UserProvider>
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="User" component={UserScreen} />
-            <Stack.Screen name="Counter" component={CounterScreen} />
-            <Stack.Screen name="Tasks" component={TaskListScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </UserProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <UserProvider>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="User" component={UserScreen} />
+              <Stack.Screen name="Counter" component={CounterScreen} />
+              <Stack.Screen name="Tasks" component={TaskListScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </UserProvider>
+      </PersistGate>
     </Provider>
   )
 }
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,15 +1,32 @@
-import {configureStore} from '@reduxjs/toolkit'
-import authSlice from './authSlice'
-import counterSlice from './counterSlice'
-import tasksSlice from './tasksSlice'
+import {configureStore, combineReducers} from '@reduxjs/toolkit'
+import {persistStore, persistReducer} from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import authReducer from './authSlice'
+import counterReducer from './counterSlice'
+import tasksReducer from './tasksSlice'
+import {thunk} from 'redux-thunk'
 
-export const store = configureStore({
-  reducer: {
-    auth: authSlice,
-    counter: counterSlice,
-    tasks: tasksSlice,
-  },
+const rootReducer = combineReducers({
+  auth: authReducer,
+  counter: counterReducer,
+  tasks: tasksReducer,
 })
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(thunk),
+})
+
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
